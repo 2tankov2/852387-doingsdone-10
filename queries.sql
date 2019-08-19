84039aa05180b9e1598daaf2acc5d250a610cc84
tankov <tankov@192.168.0.11>
2019-08-19 11:11:00 +0300

    поправки по sql-запросам

--- queries.sql
+++ queries.sql
@@ -1,21 +1,21 @@
-insert into projects (name, user_id)
-values ('Входящие', '1'), ('Учеба', 2), ('Работа', 2), ('Домашние дела', 1), ('Авто', 2);
+INSERT INTO projects (name, user_id)
+VALUES ('Входящие', '1'), ('Учеба', 2), ('Работа', 2), ('Домашние дела', 1), ('Авто', 2);
 
-insert into users (date_add, email, name, password)
-values (NOW(), 'graf45@yandex.ru', 'Гриша', 'tom789'),
-       (NOW(), 'kosmos9@mail.ru', 'Петр', 'privet');
+INSERT INTO users (email, name, password)
+VALUES ('graf45@yandex.ru', 'Гриша', 'tom789'),
+       ('kosmos9@mail.ru', 'Петр', 'privet');
 
-insert into tasks (user_id, project_id, date_add, state, name, complete_date)
-values (2, 3, NOW(), 0, 'Собеседование в IT компании', '01.12.2018'),
-       (2, 3, NOW(), 0, 'Выполнить тестовое задание', '25.12.2018'),
-       (2, 2, NOW(), 1, 'Сделать задание первого раздела', '21.12.2018'),
-       (1, 1, NOW(), 0, 'Встреча с другом', '22.12.2018'),
-       (1, 4, NOW(), 0, 'Купить корм для кота'),
-       (1, 4, NOW(), 0, 'Заказать пиццу');
+INSERT INTO tasks (user_id, project_id, state, name, complete_date)
+VALUES (2, 3, 0, 'Собеседование в IT компании', '01.12.2018'),
+       (2, 3, 0, 'Выполнить тестовое задание', '25.12.2018'),
+       (2, 2, 1, 'Сделать задание первого раздела', '21.12.2018'),
+       (1, 1, 0, 'Встреча с другом', '22.12.2018'),
+       (1, 4, 0, 'Купить корм для кота'),
+       (1, 4, 0, 'Заказать пиццу');
 
-select * from projects where user_id = 2;
+SELECT * FROM projects WHERE user_id = 2;
 
-select * from tasks where user_id = 1;
+SELECT * FROM tasks WHERE project_id = 1;
 
 UPDATE tasks SET state = 1 WHERE id = 2;
 
